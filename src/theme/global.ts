14c5c9eb408b09e55942d7631f4a3672d9bb6801
Zmiter <ozmiter@gmail.com>
2021-12-05 01:12:51 +0300

    feat: add theme styles

--- src/theme/global.ts
+++ src/theme/global.ts
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  
   // ------------------------------------------
   // Base
   //-------------------------------------------
@@ -25,6 +26,7 @@ const GlobalStyle = createGlobalStyle`
   body {
     overflow-x: hidden;
     margin: 0;
+    font-family: 'Roboto', sans-serif;
   }
 
   img {
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,11 +1,29 @@
 import { DefaultTheme } from 'styled-components';
 
 const theme: DefaultTheme = {
-  borderRadius: '5px',
   colors: {
     main: 'tomato',
-    secondary: 'white',
+    primary: '#ff3c2d',
+    secondary: '#6c757d',
+    success: '#5b0',
+    info: '#17a2b8',
+    warning: '#ffc107',
+    primaryLight: '#fff0ef',
+    danger: '#df3326',
+    indigo: '#6610f2',
+    purple: '#6f42c1',
+    pink: '#e83e8c',
+    red: '#dc3545',
+    orange: '#fd7e14',
+    yellow: '#ffc107',
+    green: '#28a745',
+    teal: '#20c997',
+    cyan: '#17a2b8',
+    white: '#fff',
+    gray: '#6c757d',
+    grayDark: '#343a40',
+    dark: '#000',
+    grayLight: '#acacac',
   },
 };
-
 export { theme };
