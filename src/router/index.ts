601c8326a90191ea35757fffab266cc455e387b0
Zmiter <ozmiter@gmail.com>
2021-12-07 17:43:05 +0300

    feat: redirect from main page

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,7 @@ export interface RoutesInterface {
 }
 
 export enum RouteNames {
+  MAIN = '/',
   LOGIN = '/login',
   PARTS = '/parts',
   DEALERS = '/dealers',
@@ -29,11 +30,14 @@ export enum RouteNames {
 }
 
 export const defaultRoutes: RoutesInterface[] = [
+  { path: RouteNames.MAIN, exact: true, component: Parts, readableName: '' },
   { path: RouteNames.LOGIN, exact: true, component: Login, readableName: '' },
   { path: RouteNames.PARTS, exact: true, component: Parts, readableName: 'Parts' },
   { path: RouteNames.DEALERS, exact: true, component: Dealers, readableName: 'Dealers' },
   { path: RouteNames.ORDERS, exact: true, component: Orders, readableName: 'Orders' },
   { path: RouteNames.PROFILE, exact: true, component: Profile, readableName: '' },
+  { path: RouteNames.CHAT, exact: true, component: Chat, readableName: '' },
+  { path: RouteNames.CART, exact: true, component: Cart, readableName: '' },
   { path: '*', exact: true, component: NotFound, readableName: '' },
 ];
 export const publicRoutes: RoutesInterface[] = [
