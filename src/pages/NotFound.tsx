3a4bf95ac96d2b60b4d5cea9a02ffa9655c51fb7
Zmiter <ozmiter@gmail.com>
2021-12-07 11:32:59 +0300

    feat: change 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,14 @@
 import React, { FC } from 'react';
+import { useLocation } from 'react-router-dom';
 import { StyledContainer } from '../theme/container';
 
 const NotFound: FC = () => {
-  return <StyledContainer>This is 404 page</StyledContainer>;
+  const location = useLocation();
+  return (
+    <StyledContainer>
+      Page <code>{location.pathname}</code> is not found{' '}
+    </StyledContainer>
+  );
 };
 
 export default NotFound;
