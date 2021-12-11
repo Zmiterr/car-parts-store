f499dc5793fe63aece23452fdf81a6a0996a8b46
Zmiter <ozmiter@gmail.com>
2021-12-11 15:26:36 +0300

    refactor: move components to other directory

--- src/pages/Cart.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react';
-import { StyledContainer } from '../theme/container';
-
-const Cart: FC = () => {
-  return <StyledContainer>This is cart page</StyledContainer>;
-};
-
-export default Cart;
---
+++ src/pages/Cart/Cart.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { Container } from '../../shared/styled/containers/Container';
+
+const Cart: FC = () => {
+  return <Container>This is cart page</Container>;
+};
+
+export default Cart;
