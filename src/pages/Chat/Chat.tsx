3ff44c9e84d210e67ed6405dd7ae3678a2c001a7
Zmiter <ozmiter@gmail.com>
2021-12-11 23:47:06 +0300

    feat: improve chat page

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -1,8 +1,25 @@
 import React, { FC } from 'react';
+import { Card } from '@material-ui/core';
 import { Container } from '../../shared/styled/containers/Container';
+import { PageHeader } from '../../shared/styled/headers/PageHeader';
+import { ChatContainer, ChatList, ChatMessages } from './styles';
 
 const Chat: FC = () => {
-  return <Container>This is chat page</Container>;
+  return (
+    <Container>
+      <PageHeader>
+        <h2>Chat</h2>
+      </PageHeader>
+      <ChatContainer>
+        <ChatList />
+        <ChatMessages>
+          <Card>
+            <p>No chat stated yet</p>
+          </Card>
+        </ChatMessages>
+      </ChatContainer>
+    </Container>
+  );
 };
 
 export default Chat;
