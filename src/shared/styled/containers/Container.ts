14f31759666809db39580fe5151c89b5b4a27751
Zmiter <ozmiter@gmail.com>
2021-12-11 15:23:28 +0300

    refactor: add container component

---
+++ src/shared/styled/containers/Container.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: block;
+  width: 100%;
+  max-width: 1200px;
+  background: #fff;
+  margin: 25px 0;
+  border-radius: 4px;
+`;
