aae1d7945ed6067f12e7b466568805def67af779
Zmiter <ozmiter@gmail.com>
2021-12-11 15:17:00 +0300

    feat: add header components

---
+++ src/shared/styled/headers/H1.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const H1 = styled.h1`
+  display: block;
+  font-size: 2.5rem;
+  line-height: 1.2;
+  word-wrap: break-word;
+  font-family: inherit;
+  color: inherit;
+`;
---
+++ src/shared/styled/headers/H5.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const H5 = styled.h5`
+  display: block;
+  font-size: 1.3rem;
+  line-height: 1.2;
+  word-wrap: break-word;
+  font-family: inherit;
+  font-weight: 700;
+  color: inherit;
+`;
