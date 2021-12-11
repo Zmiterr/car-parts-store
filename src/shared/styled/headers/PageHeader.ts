93ca6fe00eba855b478050b4c527386a99b89d76
Zmiter <ozmiter@gmail.com>
2021-12-11 23:50:55 +0300

    feat: add PageHeader styling component

---
+++ src/shared/styled/headers/PageHeader.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const PageHeader = styled.div`
+  padding: 1rem;
+  h2 {
+    display: block;
+    font-size: 2.5rem;
+    line-height: 1.2;
+    word-wrap: break-word;
+    font-family: inherit;
+    color: inherit;
+  }
+`;
