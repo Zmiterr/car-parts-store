bbf78e0f9cfa6a056bcad732ec2cc82e64bfe6d2
Zmiter <ozmiter@gmail.com>
2021-12-11 15:30:23 +0300

    feat: add component TS return type

--- src/pages/Parts/Components/Part/Part.tsx
+++ src/pages/Parts/Components/Part/Part.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC } from 'react';
 import img from '../../../../assets/ats_classic_1_large.png';
 import { H5 } from '../../../../shared/styled/headers/H5';
 import {
