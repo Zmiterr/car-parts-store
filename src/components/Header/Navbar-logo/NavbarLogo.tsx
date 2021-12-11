ef44bb756b783e2f7c224b11b5e3aa673d7b58b4
Zmiter <ozmiter@gmail.com>
2021-12-11 23:50:13 +0300

    feat: make navbar log as link

--- src/components/Header/Navbar-logo/NavbarLogo.tsx
+++ src/components/Header/Navbar-logo/NavbarLogo.tsx
@@ -1,11 +1,15 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 import { StyledImg, StyledLogo } from './styles';
 import logo from '../../../assets/logo.png';
+import { RouteNames } from '../../../router';
 
 const NavbarLogo: FC = () => {
   return (
     <StyledLogo>
-      <StyledImg src={logo} alt="Car parts store" />
+      <Link to={RouteNames.PARTS}>
+        <StyledImg src={logo} alt="Car parts store" />
+      </Link>
     </StyledLogo>
   );
 };
