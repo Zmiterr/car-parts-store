7aa40af6fc1e2d5afc339446db0699f1aaf9a6c7
Zmiter <ozmiter@gmail.com>
2021-12-11 23:49:42 +0300

    feat: improve dealers page

---
+++ src/pages/Orders/Order/Order.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { StyledCard } from '../../../shared/styled/containers/Card';
+
+const Order: FC = () => {
+  return <StyledCard>Order</StyledCard>;
+};
+
+export default Order;
--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -1,8 +1,19 @@
 import React, { FC } from 'react';
 import { Container } from '../../shared/styled/containers/Container';
+import { PageHeader } from '../../shared/styled/headers/PageHeader';
+import Order from './Order/Order';
 
 const Orders: FC = () => {
-  return <Container>This is orders page</Container>;
+  return (
+    <Container>
+      <PageHeader>
+        <h2>Orders</h2>
+        <Order />
+        <Order />
+        <Order />
+      </PageHeader>
+    </Container>
+  );
 };
 
 export default Orders;
