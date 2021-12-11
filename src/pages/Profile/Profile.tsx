200e1b0f88e1ac5bc765c8fdd99783ca863b5c92
Zmiter <ozmiter@gmail.com>
2021-12-11 23:53:23 +0300

    feat: improve profile page

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,8 +1,15 @@
 import React, { FC } from 'react';
 import { Container } from '../../shared/styled/containers/Container';
+import { PageHeader } from '../../shared/styled/headers/PageHeader';
 
 const Profile: FC = () => {
-  return <Container>This is profile page</Container>;
+  return (
+    <Container>
+      <PageHeader>
+        <h2>Profile</h2>
+      </PageHeader>
+    </Container>
+  );
 };
 
 export default Profile;
