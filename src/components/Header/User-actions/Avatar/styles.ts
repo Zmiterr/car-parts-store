1cc4c4bc80366c8074192ca6144d863f4ca0ee4e
Zmiter <ozmiter@gmail.com>
2021-12-12 10:13:54 +0300

    refactor: move images to assets/image

--- src/components/Header/User-actions/Avatar/styles.ts
+++ src/components/Header/User-actions/Avatar/styles.ts
@@ -1,14 +1,18 @@
 import styled from 'styled-components';
+// TODO fix ts error
+import emptyCart from '../../../../assets/images/user-avatar.jpg';
 
 export const Avatar = styled.div`
+  background-image: url('${emptyCart}');
   background-color: #fff;
-  background-size: 32px 32px;
+  background-position: center center;
+  background-size: 40px 40px;
   border: 0;
   border-radius: 50%;
   display: block;
   margin: 0;
   position: relative;
-  height: 32px;
-  width: 32px;
+  height: 40px;
+  width: 40px;
   cursor: pointer;
 `;
