718aa8d0be58697b6e068d76c412b31fc26f05c5
Zmiter <ozmiter@gmail.com>
2021-12-26 23:45:49 +0300

    feat: add modal styles

---
+++ src/shared/styled/containers/ModalBox.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const ModalBox = styled.div`
+  font-family: 'Roboto', sans-serif;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 400px;
+  background-color: ${(props) => props.theme.colors.white};
+`;
+
+export const CardBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+`;
