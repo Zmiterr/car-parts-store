32f4fd691ee065402a7097c43d7c2a9c81dabdd7
Zmiter <ozmiter@gmail.com>
2022-01-18 10:51:43 +0300

    refactor: remove unused component

--- src/pages/Parts/Components/Part/Styles.ts
+++ src/pages/Parts/Components/Part/Styles.ts
@@ -49,29 +49,3 @@ export const PartDealer = styled.div`
     white-space: nowrap;
   }
 `;
-
-export const PartToCartButton = styled.button`
-  font-weight: 400;
-  text-align: center;
-  white-space: nowrap;
-  vertical-align: middle;
-  user-select: none;
-  border: 1px solid transparent;
-  padding: 0.375rem 0.75rem;
-  font-size: 1rem;
-  line-height: 1.5;
-  border-radius: 4px;
-  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
-    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-  background-color: ${(props) => props.theme.colors.success};
-  &:focus {
-    text-decoration: none;
-  }
-  &:hover {
-    outline: 0;
-    text-decoration: none;
-    box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
-  }
-    
-  }
-`;
