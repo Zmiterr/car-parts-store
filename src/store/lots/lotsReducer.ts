70fcae2e0f83ce8cee5cb31f9c73b5f0ac27fe10
Zmiter <ozmiter@gmail.com>
2022-01-20 22:57:46 +0300

    refactor: comment unused code

--- src/store/lots/lotsReducer.ts
+++ src/store/lots/lotsReducer.ts
@@ -17,12 +17,12 @@ const lotsReducer = (state = initialState, action: LotsActions): LotsStateInterf
     case LotsActionsType.GET_LOTS:
       return { ...state, lots: action.payload, isLoading: false };
 
-    case LotsActionsType.REMOVE_LOT:
-      return {
-        ...state,
-        lots: state.lots.filter((lot) => lot.id !== action.payload),
-        isLoading: false,
-      };
+    // case LotsActionsType.REMOVE_LOT:
+    //   return {
+    //     ...state,
+    //     lots: state.lots.filter((lot) => lot.id !== action.payload),
+    //     isLoading: false,
+    //   };
 
     // case LotsActionsType.CREATE_LOT:
     //   return {
@@ -31,14 +31,14 @@ const lotsReducer = (state = initialState, action: LotsActions): LotsStateInterf
     //     isLoading: false,
     //   };
 
-    // TODO remove this action
-    case LotsActionsType.UPDATE_LOT:
-      return {
-        ...state,
-        // TODO write update action
-        lots: state.lots, // state.lots.find((lot: any) => lot.id === action.payload.id),
-        isLoading: false,
-      };
+    // // TODO remove this action
+    // case LotsActionsType.UPDATE_LOT:
+    //   return {
+    //     ...state,
+    //     // TODO write update action
+    //     lots: state.lots, // state.lots.find((lot: any) => lot.id === action.payload.id),
+    //     isLoading: false,
+    //   };
 
     case LotsActionsType.FETCH_LOTS_REQUEST:
       return { ...state, isLoading: true };
