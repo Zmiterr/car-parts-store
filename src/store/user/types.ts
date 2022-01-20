fd40558c14ce562239b222e6dfadc93fc9a30c7d
Zmiter <ozmiter@gmail.com>
2022-01-21 02:11:30 +0300

    feat: add user reducer

---
+++ src/store/user/types.ts
@@ -0,0 +1,34 @@
+import { Action } from 'redux';
+
+export interface UserProfileDataInterface {
+  id: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  location: string;
+}
+
+export enum UserActionsType {
+  GET_USER = 'user/GET_USER',
+  FETCH_USER_REQUEST = 'user/FETCH_USER_REQUEST',
+  FETCH_USER_ERROR = 'user/FETCH_ERROR',
+}
+
+export interface GetUserActionInterface extends Action<UserActionsType> {
+  type: UserActionsType.GET_USER;
+  payload: any;
+}
+
+export interface FetchUserRequestActionInterface extends Action<UserActionsType> {
+  type: UserActionsType.FETCH_USER_REQUEST;
+}
+
+export interface FetchUserErrorActionInterface extends Action<UserActionsType> {
+  type: UserActionsType.FETCH_USER_ERROR;
+}
+
+export type UserActions =
+  | GetUserActionInterface
+  | FetchUserRequestActionInterface
+  | FetchUserErrorActionInterface;
