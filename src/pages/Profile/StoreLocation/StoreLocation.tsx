446d543890ce479e3384466016e169289ef3a2d6
Zmiter <ozmiter@gmail.com>
2022-02-12 21:07:21 +0300

    feat: add location settings

--- src/pages/Profile/StoreLocation/StoreLocation.tsx
+++ src/pages/Profile/StoreLocation/StoreLocation.tsx
@@ -1,16 +1,29 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Placemark, YMaps, Map } from 'react-yandex-maps';
 
 const StoreLocation: FC = () => {
+  const [place, setPlace] = useState([55.684758, 37.738521]);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setPlace([position.coords.latitude, position.coords.longitude]);
+    });
+  }, []);
+
+  const clickOnMap = (e: { get: (arg0: string) => React.SetStateAction<number[]> }) => {
+    setPlace(e.get('coords'));
+  };
   return (
     <YMaps>
       <Map
+        width="90vh"
+        height="60vh"
         defaultState={{
-          center: [55.751574, 37.573856],
-          zoom: 5,
+          center: place,
+          zoom: 13,
         }}
+        instanceRef={(inst) => inst?.events?.add('click', clickOnMap)}
       >
-        <Placemark geometry={[55.684758, 37.738521]} />
+        <Placemark geometry={place} properties={{ iconCaption: 'your store' }} />
       </Map>
     </YMaps>
   );
