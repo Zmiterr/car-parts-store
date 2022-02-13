b17ad4c98d2f1e7d0bf1754606a38aa97e68fc5b
Zmiter <ozmiter@gmail.com>
2022-02-13 16:19:59 +0300

    feat: add controls to map

--- src/pages/Profile/StoreLocation/StoreLocation.tsx
+++ src/pages/Profile/StoreLocation/StoreLocation.tsx
@@ -1,19 +1,30 @@
 import React, { FC, useEffect, useState } from 'react';
-import { Placemark, YMaps, Map } from 'react-yandex-maps';
+import {
+  Placemark,
+  YMaps,
+  Map,
+  ZoomControl,
+  FullscreenControl,
+  GeolocationControl,
+  SearchControl,
+} from 'react-yandex-maps';
 
 const StoreLocation: FC = () => {
   const [place, setPlace] = useState([55.684758, 37.738521]);
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function (position) {
+    navigator.geolocation.getCurrentPosition((position) => {
       setPlace([position.coords.latitude, position.coords.longitude]);
     });
   }, []);
+  const [ymaps, setYmaps] = useState();
 
   const clickOnMap = (e: { get: (arg0: string) => React.SetStateAction<number[]> }) => {
     setPlace(e.get('coords'));
+    console.log(ymaps);
+    const geoObjects = ymaps && ymaps.geocode(e.get('coords'));
   };
   return (
-    <YMaps>
+    <YMaps query={{ load: 'package.full' }}>
       <Map
         width="90vh"
         height="60vh"
@@ -22,8 +33,13 @@ const StoreLocation: FC = () => {
           zoom: 13,
         }}
         instanceRef={(inst) => inst?.events?.add('click', clickOnMap)}
+        onLoad={(maps) => setYmaps(maps)}
       >
         <Placemark geometry={place} properties={{ iconCaption: 'your store' }} />
+        <ZoomControl />
+        <FullscreenControl />
+        <SearchControl />
+        <GeolocationControl />
       </Map>
     </YMaps>
   );
