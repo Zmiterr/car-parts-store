225d10b3bf88556a6eddc8bdeebe0a9da21abc89
Zmiter <ozmiter@gmail.com>
2022-02-13 15:56:56 +0300

    feat: add compare functional to parts page

--- src/pages/Parts/Components/Part/Part.tsx
+++ src/pages/Parts/Components/Part/Part.tsx
@@ -1,4 +1,8 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Checkbox, FormControlLabel } from '@mui/material';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import BookmarkIcon from '@mui/icons-material/BookmarkAdded';
 import img from '../../../../assets/images/ats_classic_1_large.png';
 import { H5 } from '../../../../shared/styled/headers/H5';
 import {
@@ -14,14 +18,34 @@ import { StyledCard } from '../../../../shared/styled/containers/Card';
 import { SmallDescribe } from '../../../../shared/styled/headers/SmallDescribe';
 import { PrimaryButton } from '../../../../shared/styled/Elements/PrimaryButton';
 import { LotInterface } from '../../../../store/lots/types';
+import { addLotToCompare, removeLotFromCompare } from '../../../../store/lots/lotsActions';
+import { useTypedSelector } from '../../../../hooks/useTypedSelector';
 
 interface PartPropsInterface {
   lot: LotInterface;
 }
 
 const Part: FC<PartPropsInterface> = ({ lot }) => {
+  const dispatch = useDispatch();
+
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCompareClick = () => {
+    const updateLotCompare = isChecked ? removeLotFromCompare : addLotToCompare;
+    dispatch(updateLotCompare(lot.id));
+    setIsChecked(!isChecked);
+  };
+
+  const lotsToCompare = useTypedSelector((state) => state.lots.lotsToCompare);
+
   return (
     <StyledCard>
+      <FormControlLabel
+        control={<Checkbox icon={<BookmarkBorderIcon />} checkedIcon={<BookmarkIcon />} />}
+        label=""
+        checked={lotsToCompare.some((checkedLot) => lot.id === checkedLot)}
+        labelPlacement="start"
+        onChange={handleCompareClick}
+      />
       <PartImage src={img} alt="text" />
       <PartSpecifications>
         <H5>{lot.name}</H5>
