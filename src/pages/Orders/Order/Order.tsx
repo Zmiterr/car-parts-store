db570a6c925b39823247cd268b323be445b25864
Zmiter <ozmiter@gmail.com>
2022-02-20 02:12:31 +0300

    feat: add simple orders dealer page

--- src/pages/Orders/Order/Order.tsx
+++ src/pages/Orders/Order/Order.tsx
@@ -1,8 +1,25 @@
 import React, { FC } from 'react';
 import { StyledCard } from '../../../shared/styled/containers/Card';
+import { OrderInterface } from '../../../store/orders/types';
+import { LotInterface } from '../../../store/lots/types';
 
-const Order: FC = () => {
-  return <StyledCard>Order</StyledCard>;
+interface OrderProps {
+  order: OrderInterface;
+  lotsData: LotInterface[];
+}
+
+const Order: FC<OrderProps> = ({ order, lotsData }) => {
+  const orderLotDetail = lotsData.find((lot) => order.id === lot.id);
+  return (
+    <StyledCard>
+      <img src={orderLotDetail?.defaultImageUrl} alt={orderLotDetail?.description} />
+      <p>
+        Customer: {order.customerFirstName} {order.customerLastName}
+        <br />
+        Contacts: {order.phone}, {order.email}
+      </p>
+    </StyledCard>
+  );
 };
 
 export default Order;
--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -1,17 +1,30 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Container } from '../../shared/styled/containers/Container';
 import { PageHeader } from '../../shared/styled/headers/PageHeader';
 import Order from './Order/Order';
+import { getOrders } from '../../store/orders/ordersActions';
+import { useTypedSelector } from '../../hooks/useTypedSelector';
+import { getLots } from '../../store/lots/lotsActions';
 
 const Orders: FC = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getOrders());
+    dispatch(getLots());
+  }, [dispatch]);
+
+  const orders = useTypedSelector((state) => state.orders.orders);
+  const lots = useTypedSelector((state) => state.lots.lots);
+  const lotsIdArray = lots.map((lot) => lot.id);
+  const lotsData = lots.filter((lot) => lotsIdArray.includes(lot.id));
+
   return (
     <Container>
       <PageHeader>
         <h2>Orders</h2>
       </PageHeader>
-      <Order />
-      <Order />
-      <Order />
+      {orders && orders.map((order) => <Order key={order.id} order={order} lotsData={lotsData} />)}
     </Container>
   );
 };
