c7b4a14b69e1b97b64f3208ee4c22a9fe6c861c5
Zmiter <ozmiter@gmail.com>
2022-03-02 20:44:37 +0300

    fix: change pagination

--- src/pages/Chat/ChatRoom/ChatRoom.tsx
+++ src/pages/Chat/ChatRoom/ChatRoom.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { ChatListItem } from '../styles';
+import { RouteNames } from '../../../router';
 
 interface ChatRoomProps {
   id: number;
@@ -15,7 +16,7 @@ const ChatRoom: FC<ChatRoomProps> = ({ addressId, chatName, chatImageUrl, id, se
     setActiveTabId(id);
   };
   return (
-    <Link key={id} to={`chat/${id}`} onClick={handleClick}>
+    <Link key={id} to={`${RouteNames.CHAT.replace(`:id`, id)}`} onClick={handleClick}>
       <ChatListItem isActiveTab={addressId === id}>
         <img src={chatImageUrl} alt={chatName} />
         <div>
